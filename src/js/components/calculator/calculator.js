90932550cf3470334b083ea6a45feae98da2d50c
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2021-02-09 14:57:14 +0300

    правки в функционале калькулятора

--- src/js/components/calculator/calculator.js
+++ src/js/components/calculator/calculator.js
@@ -38,7 +38,7 @@ export class Calculator {
 
       if (sumVal > this.JSON.values.maxPrice) {
          newValue = this.JSON.values.maxPrice;
-      } else if (sumVal < this.JSON.values.minPrice) {
+      } else if (sumVal < this.JSON.values.minPrice || !sumVal) {
          newValue = this.JSON.values.minPrice;
       }
 
@@ -50,9 +50,9 @@ export class Calculator {
 
       const initialFeeSlider = $('[name="mortgage_sum_range"]').data('ionRangeSlider');
 
-      if (value > this.JSON.values.maxPrice) {
+      if (newVal > this.JSON.values.maxPrice) {
          newVal = this.JSON.values.maxPrice;
-      } else if (value < this.JSON.values.minPrice) {
+      } else if (newVal < this.JSON.values.minPrice || !newVal) {
          newVal = this.JSON.values.minPrice;
       }
 
@@ -73,27 +73,6 @@ export class Calculator {
       $('#calc-close-paid').text(toCurrency(closePaid))
       $('#calc-close-paid-crossed').text(toCurrency(closePaidCrossed))
       $('#calc-commission-paid').text(toCurrency(commissionPaid))
-
-/*
-      let dateString = this.getDateString(termVal);
-      let percentPerDay = 0.01;
-
-      let totalVal = roundedSum + roundedSum * (termVal * percentPerDay);
-
-      $('#calculator-sum-get').text(numberFormat(roundedSum, 0, '', ' ') + ' ₽');
-      $('#calculator-sum-get__input').val(roundedSum);
-      $('#calculator-sum-term').text(termVal + ` ${declOfNum(termVal, ['день', 'дня', 'дней'])}`);
-      $('#calculator-sum-term__input').val(termVal);
-      $('#calculator-sum-date').text(dateString);
-      $('#calculator-sum-date__input').val(dateString);
-      $('#calculator-sum-total').text(numberFormat(totalVal, 0, '', ' ') + ' ₽');
-      $('#calculator-sum-total__input').val(totalVal);*/
-
-      //this.$sumInput.val(roundedSum)
-
-      //this.$sendBtn.attr('href', `https://new.kredito24.ru/entry?total_amount=${roundedSum}&number_of_installments=${termVal}&product_type=K24`);
-      //this.$sendBtn.attr('href', `http://kredito24.pro/start/credit?amount=${roundedSum}&period=${termVal}&periodUnit=DAYS&creditProductId=345&utm`);
-      //this.$sendBtn.attr('href', ` https://lk.kredito24.pro/start/credit?amount=${roundedSum}&period=${termVal}&periodUnit=DAYS&creditProductId=345&utm`);
    };
 
    initSumRangeSlider = () => {
@@ -102,30 +81,22 @@ export class Calculator {
       mortgageSumInput[0].value = numberFormat(this.JSON.default.price, 0, ' ');
       this.$sumRangeInput[0].value = this.JSON.default.price;
 
-      mortgageSumInput
-         .closest('.calculator-item')
-         .find('.calculator-min')
-         .text(numberFormat(this.JSON.values.minPrice, 0, ' ') + ' ₽');
-      mortgageSumInput
-         .closest('.calculator-item')
-         .find('.calculator-max')
-         .text(numberFormat(this.JSON.values.maxPrice, 0, ' ') + ' ₽');
 
       return $('[name="mortgage_sum_range"]').ionRangeSlider({
          type: 'single',
          min: this.JSON.values.minPrice,
          max: this.JSON.values.maxPrice,
          from: this.JSON.default.price,
-         step: 100,
+         step: 1000,
          drag_interval: true,
          prettify_enabled: true,
          prettify_separator: ' ',
          onChange: data => {
-            mortgageSumInput[0].value = data.from_pretty;
-            this.$sumRangeInput[0].value = data.from_pretty;
+            mortgageSumInput[0].value = data.from;
+            this.$sumRangeInput[0].value = data.from;
          },
          onUpdate: data => {
-            this.$sumRangeInput[0].value = data.from_pretty;
+            this.$sumRangeInput[0].value = data.from;
             setTimeout(() => {
                this.getCalculatorSummary();
             }, 10);
